8689157504cec48cf17925302a425a19a0afb23d
Lee Junho <dangercloz@gmail.com>
2017-01-07 20:40:03 +0900

    Initial commit

---
+++ ArchitectureStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArchitectureStudy
+//
+//  Created by Lee Junho on 2017. 1. 7..
+//  Copyright © 2017년 Lee Junho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
